ad89e072bb70e63fba2fea092f04714a7e9c8b22
asa-lab-bit <asakai.iso@gmail.com>
2026-04-20 15:24:59 +0900

    Create AQM1602_SoftI2C.h

---
+++ AQM1602_SoftI2C.h
@@ -0,0 +1,26 @@
+#ifndef AQM1602_SOFTI2C_H
+#define AQM1602_SOFTI2C_H
+
+#include <Arduino.h>
+#include <SoftI2C_VPORT.h>
+
+class AQM1602 {
+  public:
+    AQM1602(SoftI2C_VPORT& i2c);
+
+    void begin(uint8_t contrast = 0x72, uint8_t power = 0x52);
+    void clear();
+    void setCursor(uint8_t col, uint8_t row);
+
+    void print(const char* text);
+    void print(int value);
+    void printAt(uint8_t col, uint8_t row, const char* text);
+
+  private:
+    SoftI2C_VPORT& _i2c;
+
+    void sendCommand(uint8_t cmd);
+    void sendData(uint8_t data);
+};
+
+#endif
