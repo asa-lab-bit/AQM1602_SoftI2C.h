d2e89c6e121665fa8a90b7f2b1d1f3e4e951cf7b
asa-lab-bit <asakai.iso@gmail.com>
2026-04-20 15:40:55 +0900

    Create Basic.ino

---
+++ examples/Basic/Basic.ino
@@ -0,0 +1,21 @@
+#include <SoftI2C_VPORT.h>
+#include "AQM1602.h"
+
+// ★ I2Cをここで作る
+SoftI2C_VPORT i2c(2, 3);  //使用するPinを設定する
+
+// ★ 渡す
+AQM1602 lcd(i2c);
+
+void setup() {
+  lcd.begin(0x70, 0x56);  //コントラスト設定  電圧設定3.3V向け
+  int aa= -23;
+  lcd.printAt(2, 0, "Hello");
+  lcd.printAt(8, 0, "Hello");
+  lcd.setCursor(0,1);
+  lcd.print(aa);
+  lcd.print(" abcdef");
+}
+
+void loop() {
+}
