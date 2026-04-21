f55e120b574de7717e4718739e19e83e9e2c5653
asa-lab-bit <asakai.iso@gmail.com>
2026-04-21 12:29:15 +0900

    Update Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -8,7 +8,7 @@ SoftI2C_VPORT i2c(1, 2);  //使用するPinを設定する SDA=PA1, SCL=PA2
 AQM1602 lcd(i2c);
 
 void setup() {
-  lcd.begin(0x70, 0x56);  //コントラスト設定  電圧設定3.3V向け
+  lcd.begin(0x73, 0x56);  //コントラスト設定  電圧設定3.3V向け
   int aa= -23;
   lcd.printAt(2, 0, "Hello");
   lcd.printAt(8, 0, "Hello");
